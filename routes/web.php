94716685cfd185e08a0674d1d51d27f0c3346b65
Syahir Dev <888syahir@gmail.com>
2020-08-30 19:42:38 +0800

    Passing data to views
    Data from routes folder (web.php) sending data to resources>views folder (pizzas.blade.php)

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pizzas', function () {
+    $pizza = [
+        'type' => 'Hawaiian',
+        'base' => 'Cheesy Crust',
+        'price' => 10
+    ];
+
+    return view('pizzas', $pizza);
+});
