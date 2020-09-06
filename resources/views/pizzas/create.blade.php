4c1ac1dcd7e5875d0b05d7a54194733e16ee0c12
Syahir Dev <888syahir@gmail.com>
2020-09-06 23:47:13 +0800

    Creating a Web Form
    create new simple form for ordering new pizza in /pizzas/create routes.
    Sending the POST request form to /pizzas

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -1,15 +1,28 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="flex-center position-ref full-height">
+<div class="wrapper create-pizza">
+    <h1>Create a New Pizza</h1>
+    <form action="" method="">
+        <label for="name">Your Name:</label>
+        <input type="text" id="name" name="name">
 
-    <div class="content">
-        <div class="title m-b-md">
-            Add a New Pizza
-        </div>
+        <label for="type">Choose Pizza Type:</label>
+        <select name="type" id="type">
+            <option value="margarita">Margarita</option>
+            <option value="hawaiian">Hawaiian</option>
+            <option value="vegies">Vegies</option>
+            <option value="volcano">Volcano</option>
+        </select>
 
-
-
-    </div>
+        <label for="base">Choose Pizza Base:</label>
+        <select name="base" id="base">
+            <option value="cheesy crust">Cheesy Crust</option>
+            <option value="garlic crust">Garlic Crust</option>
+            <option value="thin and crispy">Thin & Crispy</option>
+            <option value="thick">Thick</option>
+        </select>
+        <input type="submit" value="Order Pizza">
+    </form>
 </div>
 @endsection
